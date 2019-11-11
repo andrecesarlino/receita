dae8a2db8eadfc6d34430e979feefabad09fdbf1
Andre <andrelino@unidavi.edu.br>
2019-11-11 15:30:13 -0300

    Migrate funcionando

---
+++ database/migrations/2019_11_12_175915_create_pessoas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePessoasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_cidade')->unsigned();
+            $table->string('nome',150);
+            $table->string('endereco',150);
+            $table->foreign('id_cidade')->references('id')->on('cidades')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoas');
+    }
+}
